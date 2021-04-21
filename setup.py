a566eabdd937f2b4fe8f381f156582f5cb0fc3fb
Fredrick Brennan <copypaste@kittens.ph>
2021-04-21 13:52:25 -0400

    Added to PyPI
    https://pypi.org/project/python-atd/0.1/
    
    Closes #6.

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup
 
-#def readme():
- #   with open('README.rst') as f:
-  #      return f.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(name='python-atd',
       version='0.1',
-      description='Unix `at` scheduler in Python 2. Supports atq, atd, & atrm',
+      description='Unix `at` scheduler, for Python 3 and Python 2. Supports atq, atd, & atrm',
       keywords='atq atrm atd at',
-      #long_description=readme(),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/ctrlcctrlv/python-atd',
       author='Fredrick Brennan',
-      author_email='admin@8chan.co',
+      author_email='copypaste@kittens.ph',
       license='MIT',
       packages=['atd'],
       test_suite='nose.collector',
       tests_require=['nose'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      package_dir={"":"."},
       zip_safe=False)
