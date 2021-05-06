5aa2b11770c20bcc5dfb5d03ba171416e10fb90d
musover <meoberto@mthree.es>
2021-05-06 13:51:02 +0200

    Fixed setup.py for Python 2 compatibility

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
+import io
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with io.open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='python-atd',
